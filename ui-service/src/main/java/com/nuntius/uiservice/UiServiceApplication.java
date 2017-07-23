97a72eeabfc508b80c32f5efdb0071d6ebbd1c29
SergioJF <sjf3@alu.ua.es>
2017-07-23 13:31:22 +0200

    Fixes

--- ui-service/src/main/java/com/nuntius/uiservice/UiServiceApplication.java
+++ ui-service/src/main/java/com/nuntius/uiservice/UiServiceApplication.java
@@ -1,11 +1,15 @@
 package com.nuntius.uiservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @EnableDiscoveryClient
 @SpringBootApplication
 public class UiServiceApplication {
