9ed25e756d480ddc980188a5ab3b7aee74e38b10
SergioJF <sjf3@alu.ua.es>
2017-07-28 19:10:13 +0200

    Fixes

--- config/src/main/java/com/nuntius/config/ConfigApplication.java
+++ config/src/main/java/com/nuntius/config/ConfigApplication.java
@@ -1,16 +1,14 @@
-package com.nuntius.configserver;
+package com.nuntius.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@EnableAutoConfiguration
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(ConfigApplication.class, args);
 	}
 }
