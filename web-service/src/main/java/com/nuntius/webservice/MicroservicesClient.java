099280d139ba185741dded41d61c2a217ea5a5ef
SergioJF <sjf3@alu.ua.es>
2017-08-23 21:57:23 +0200

    Fixes

--- web-service/src/main/java/com/nuntius/webservice/MicroservicesClient.java
+++ web-service/src/main/java/com/nuntius/webservice/MicroservicesClient.java
@@ -5,7 +5,7 @@ import org.springframework.hateoas.PagedResources;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("zuulserver")
+@FeignClient("edge-service")
 public interface MicroservicesClient {
 
 	@RequestMapping(method = RequestMethod.GET, value = "/comments")
