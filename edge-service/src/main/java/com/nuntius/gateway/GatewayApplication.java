fde20b68a98f2baf9f4bedfa7e2085fc1b8f9f28
SergioJF <sjf3@alu.ua.es>
2017-08-24 18:19:22 +0200

    fixes

--- edge-service/src/main/java/com/nuntius/gateway/GatewayApplication.java
+++ edge-service/src/main/java/com/nuntius/gateway/GatewayApplication.java
@@ -1,13 +1,20 @@
 package com.nuntius.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+@Configuration
+@ComponentScan
 public class GatewayApplication {
 
 	public static void main(String[] args) {
